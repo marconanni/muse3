1abe18e254bd9aa499a5f4f3c345275f19392a3d
pire.dejaco <pire.dejaco@0b1e5f34-49f6-11de-a65c-33beeba39556>
2009-09-29 10:50:31 +0000

    Server versione 1, cambiato la console di debug

---
+++ trunk/Muse_Pire/src/server/connection/ServerCM.java
@@ -0,0 +1,31 @@
+package server.connection;
+
+import java.net.InetAddress;
+import java.util.Observer;
+
+import parameters.Parameters;
+
+import client.connection.ConnectionReceiverAndSender;
+import debug.DebugConsole;
+
+/**
+ * @author Luca Campeti
+ *
+ */
+public class ServerCM  {
+	
+	AConnectionReceiver crs = null;
+	Thread serverCMThread = null;
+	DebugConsole console;
+	
+	public ServerCM(String localAddress,int receivingPort, Observer observer, DebugConsole console){
+		crs = new AConnectionReceiver(observer,localAddress, receivingPort);
+		serverCMThread = new Thread(crs);
+		this.console=console;
+	}
+	
+	public void start(){
+		if(serverCMThread!=null)serverCMThread.start();
+		console.debugMessage(Parameters.DEBUG_INFO,"ServerCM partito");
+	}
+}
