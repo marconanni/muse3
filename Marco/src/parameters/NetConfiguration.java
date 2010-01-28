dc51f44544e6316c3d136178908cfcd68e0c6fc9
marco.nanni0 <marco.nanni0@0b1e5f34-49f6-11de-a65c-33beeba39556>
2010-01-28 17:05:35 +0000

    Messi i parametri come ha fatto pire, ma desso è un gran casino sistemare tutto...

---
+++ Marco/src/parameters/NetConfiguration.java
@@ -0,0 +1,57 @@
+package parameters;
+
+public class NetConfiguration {
+	
+	/*
+	 ********************* CONFIGURAZIONE RELAY *****************************
+	 * 
+	 * */	
+	/**Discriminazione di due tipi di relay
+	 * relay principale (collegato col nodo server) -> BIG BOSS	IMRELAY = true && IMBIGBOSS = true
+	 * relay secondario (collegato col nodo BIG BOSS) 			IMRELAY = true && IMBIGBOSS = false
+	 */
+	public static final boolean IMRELAY=false;
+	public static final boolean IMBIGBOSS=false;
+	public static final boolean IMPOSSIBLEBIGBOSS = false;
+	public static final boolean IMPOSSIBLERELAY = false;
+	public static final boolean IMCLIENT = false;
+	
+	/*
+	 ********************* CONFIGURAZIONE DELLA RETE *****************************
+	 * 
+	 * */	
+	/** SERVER e BIGBOSS*/
+	public static final String SERVER_ADDRESS = "192.168.186.230";					//indirizzo IP del SERVE
+	public static final String NAME_OF_SERVER_INTERFACE = "eth0";					//Nome dell'interfaccia con cui il Server si affaccia sulla rete Managed (cablato all'AP) 
+	
+	//nel caso di big boss questi parametri sono uguali ai parametri del cluster head
+	public static final String NAME_OF_RELAY_MANAGED_NETWORK = "ALMAWIFI";				//Nome della rete Managed
+	public static final String NAME_OF_RELAY_MANAGED_WIFI_INTERFACE  = "wlan0";			//Nome dell'interfaccia con cui il Relay si affaccia sulla rete Managed
+	public static final String RELAY_MANAGED_ADDRESS = "192.168.186.230";				//indirizzo IP del RELAY sulla rete Managed (BigBoss)
+	public static final String RELAY_MANAGED_BROADCAST_ADDRESS = "192.168.187.255";
+	
+	/** CLUSTER HEAD*/
+	public static final String NAME_OF_RELAY_CLUSTER_HEAD_NETWORK = "BIGBOSS";		//Nome della rete Ad-Hoc (cluster head)
+	public static final String NAME_OF_RELAY_CLUSTER_HEAD_WIFI_INTERFACE = "wlan1";//Nome dell'interfaccia con cui il Relay si affaccia sulla rete Ad-Hoc (cluster head)
+	public static final String RELAY_CLUSTER_HEAD_ADDRESS = "192.168.30.3";	//indirizzo IP del RELAY nella rete Ad-Hoc di competenza del BigBoss (cluster head)
+	public static final String RELAY_CLUSTER_HEAD_BROADCAST_ADDRESS = "192.168.30.255";	//indirizzo IP di BROADCAST sulla rete Ad-Hoc (cluster head)
+	
+	/** CLUSTER */
+	public static final String NAME_OF_RELAY_CLUSTER_NETWORK = "RELAY1";			//Nome della rete Ad-Hoc (cluster) 
+	public static final String NAME_OF_RELAY_CLUSTER_WIFI_INTERFACE = "wlan0";		//Nome dell'interfaccia con cui il Relay si affaccia sulla rete Ad-Hoc (cluster)
+	public static final String RELAY_CLUSTER_ADDRESS = "localhost";		//indirizzo IP del RELAY nella rete Ad-Hoc di sua competenza (cluster) non server al bigboss
+	public static final String RELAY_CLUSTER_BROADCAST_ADDRESS = "192.168.5.255"; 		//indirizzo IP di BROADCAST sulla rete Ad-Hoc (cluster)
+	
+	/** CLIENT */
+	public static final String NAME_OF_CLIENT_NETWORK = "lord";
+	public static final String NAME_OF_CLIENT_WIFI_INTERFACE = "wlan0";				//Nome dell'interfaccia con cui il Client si affaccia sulla rete Ad-Hoc
+	public static final String CLIENT_ADDRESS = "192.168.2.4";						//indirizzo IP del CLIENT
+	public static final String CLIENT_BROADCAST_ADDRESS = "192.168.2.255";
+	
+	
+
+
+	
+	
+
+}
---
+++ Marco/src/parameters/TimeOutConfiguration.java
@@ -0,0 +1,61 @@
+package parameters;
+
+public class TimeOutConfiguration {
+
+	/** Periodo con cui il RelayPositionClientsMonitor richiede ai Clients serviti il valore di RSSI rilevato nei suoi confronti */
+	public static final long POSITION_CLIENTS_MONITOR_PERIOD = 4000;
+	
+	/** Periodo con cui il RelayPositionAPMonitor richiede al RelayWNICController il valore di RSSI rilevato nei confronti dell'AP */
+	public static final long POSITION_AP_MONITOR_PERIOD = 4000;
+		
+	/** Periodo con cui il RelayBatteryMonitor rileva la carica rimanente nella batteria */
+	public static final long BATTERY_MONITOR_PERIOD = 4000;
+	
+	/** timeout relativo al messaggio WHO_IS_RELAY */
+	public static final int TIMEOUT_SEARCH = 10000;
+	public static final String TIME_OUT_SEARCH = "TIMEOUTSEARCH";
+		
+	/** timeout relativo all'intera fase di elezione */
+	public static final int TIMEOUT_FAIL_TO_ELECT = 4000;
+	public static final String TIME_OUT_FAIL_TO_ELECT = "TIMEOUTFAILTOELECT";
+	
+	/** timeout relativo al messaggio FILE_REQ */
+	public static final int TIMEOUT_FILE_REQUEST = 35000;
+	public static final String TIME_OUT_FILE_REQUEST = "TIMEOUTFILEREQUEST";
+		
+	/** timeout relativo all'intera fase di elezione */
+	public static final int TIMEOUT_TO_ELECT = 3000;
+	public static final String TIME_OUT_TO_ELECT = "TIMEOUTTOELECT";
+	
+	/** timeout relativo al messaggio di risposta dei client al messagigo ELECTION_REQUEST */
+	public static final int TIMEOUT_ELECTION_BEACON = 1000;
+	public static final String TIME_OUT_ELECTION_BEACON = "TIMEOUTELECTIONBEACON";
+	
+	/** timeout relativo alla prima fase del protocollo di emergenza */
+	public static final int TIMEOUT_CLIENT_DETECTION = 2000;
+	
+	/** timeout relativo all'intera fase di elezione d'emergenza */
+	public static final int TIMEOUT_EM_ELECTION = 4000;
+	
+	/** timeout relativo all'attesa del messaggio di SESSION_REQUEST */
+	public static final int TIMEOUT_SESSION_REQUEST = 1000;
+	
+	/** timeout relativo al messaggio di SESSION_INFO */
+	public static final int TIMEOUT_SESSION_INFO = 1000;
+	
+	/** timeout relativo al messaggio di ACK_SESSION_INFO */
+	public static final int TIMEOUT_ACK_SESSION_INFO = 2000;
+	
+	/** timeout relativo al messaggio ACK_CLIENT_REQ */
+	public static final int TIMEOUT_ACK_CLIENT_REQ = 200000;
+	
+	/** timeout relativo all'interruzione della sessione da parte del client */
+	public static final int TIMEOUT_SESSION_INTERRUPTED = 120000;
+	
+	/** timeout relativo al messaggio di ACK_FORWARD */
+	public static final int TIMEOUT_ACK_FORWARD = 60000;
+	
+	/** timeout relativo al messaggio di NOTIFY_RSSI */
+	public static final int TIMEOUT_NOTIFY_RSSI = 1000;
+	public static final String TIME_OUT_NOTIFY_RSSI = "TIMEOUTNOTIFYRSSI";
+}
