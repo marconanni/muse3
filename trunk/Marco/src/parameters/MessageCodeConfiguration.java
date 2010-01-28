b29bb52702148c957acef871bfafaa51db953e7e
marco.nanni0 <marco.nanni0@0b1e5f34-49f6-11de-a65c-33beeba39556>
2010-01-28 17:05:35 +0000

    Messi i parametri come ha fatto pire, ma desso è un gran casino sistemare tutto...

---
+++ trunk/Marco/src/parameters/MessageCodeConfiguration.java
@@ -0,0 +1,46 @@
+package parameters;
+
+public class MessageCodeConfiguration {
+	
+	public static final int TYPERELAY = 0;
+	public static final int TYPECLIENT = 1;
+	public static final int TYPERELAYPASSIVE = 2;
+	
+	public static final int WHO_IS_RELAY = 3;
+	public static final int IM_RELAY = 4;
+	
+	public static final int ACK_CONNECTION = 5;
+
+	public static final int REQUEST_RSSI = 6;
+	public static final int NOTIFY_RSSI = 7;
+	
+	public static final int ELECTION_REQUEST = 11;			//Messaggio inviato da un relay attualmente attivo che deve essere sostituito
+	public static final int ELECTION_BEACON = 12;			//Messaggio inviato dai client con sessione RTP in corso destinato ai possibili relay sostituti
+	public static final int ELECTION_BEACON_RELAY = 13;
+	public static final int ELECTION_RESPONSE = 14;
+	public static final int ELECTION_DONE = 16;				//Messagio inviato dal nuovo relay appena eletto
+	
+	public static final int EM_EL_DET_CLIENT = 6;
+	public static final int EM_EL_DET_RELAY = 7;
+	public static final int EM_ELECTION = 8;
+	
+	public static final int REQUEST_FILE = 14;
+	public static final int FORWARD_REQ_FILE = 15;		//Richiesta file al server
+	public static final int ACK_RELAY_FORW = 16;
+	public static final int ACK_CLIENT_REQ = 17;
+	
+	public static final int REQUEST_SESSION= 9;
+	public static final int SESSION_INFO = 10;
+	public static final int ACK_SESSION = 11;
+	public static final int REDIRECT = 12;
+	public static final int LEAVE = 13;
+	
+	public static final int START_TX = 18;
+	public static final int STOP_TX = 19;
+	
+	public static final int ACK = 22;	
+	
+	public static final int SERVER_UNREACHEABLE = 23;
+	public static final int SESSION_INVALIDATION = 24;
+
+}
