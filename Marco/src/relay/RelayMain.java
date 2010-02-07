61efb87fc4d5d84e4e6c9860269249f92e6f8fe7
marco.nanni0 <marco.nanni0@0b1e5f34-49f6-11de-a65c-33beeba39556>
2010-02-07 17:18:43 +0000

    Sostituito il flag Im' relay ed i relativi getters setters, appartenente alla vecchia versione ed inutile nella nuova. Iniziato a riguardare il codice seguendo il protocollo, arrivato all'arrivo del REQUEST SESSION

--- Marco/src/relay/RelayMain.java
+++ Marco/src/relay/RelayMain.java
@@ -9,11 +9,11 @@ public class RelayMain {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		boolean imRelay = Parameters.IMRELAY;
+		
 		RelaySessionManager sessionManager = null;
 		RelayElectionManager electionManager = null;
 		sessionManager = RelaySessionManager.getInstance();
-		sessionManager.setImRelay(imRelay);
+		
 		electionManager = RelayElectionManager.getInstance(imRelay, sessionManager);
 	//	electionManager.addObserver(sessionManager);
 		sessionManager.setElectionManager(electionManager);
